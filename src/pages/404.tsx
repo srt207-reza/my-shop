42d5efa08627d9938647bda5c1d65cc2e4129483
Reza Taghizadeh <95388378+srt207-reza@users.noreply.github.com>
2024-03-20 08:18:15 +0330

    add file

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Custom404: NextPage = () => {
+  return (
+    <h2 style={{ textAlign: "center", margin: "10% 0" }}>
+      صفحه مورد نظر یافت نشد !
+    </h2>
+  );
+};
+
+export default Custom404;
